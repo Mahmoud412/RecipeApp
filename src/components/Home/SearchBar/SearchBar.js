70e49c4ff31f8fb68c12587484b5b15820ae4a6d
Mahmoud412 <mahmoudashour219@gmail.com>
2023-02-07 17:22:08 +0300

    added Search Function

--- src/components/Home/SearchBar/SearchBar.js
+++ src/components/Home/SearchBar/SearchBar.js
@@ -1,9 +1,35 @@
-import {View, Text, TextInput, StyleSheet, SafeAreaView} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  SafeAreaView,
+  Button,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import styles from './style';
-
+import {baseUrl, options} from '../../../service/Api';
 const SearchBar = () => {
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
+
+  const handleSearch = async text => {
+    setQuery(text);
+
+    const response = await fetch(
+      `${baseUrl}/auto-complete?prefix=${text}`,
+      options,
+    );
+    const data = await response.json();
+    setResults(data.results);
+  };
+
+  useEffect(() => {
+    handleSearch();
+  }, []);
   return (
     <SafeAreaView>
       <Text style={styles.text1}>Delicious</Text>
@@ -14,9 +40,40 @@ const SearchBar = () => {
           <TextInput
             style={styles.textInput}
             placeholder="Search your perfect recipe"
+            value={query}
+            onChangeText={text => handleSearch(text)}
           />
         </View>
       </View>
+      {query ? (
+        <View>
+          <FlatList
+            style={{
+              borderRadius: 10,
+              backgroundColor: 'white',
+              shadowOffset: {
+                width: 0,
+                height: 3,
+              },
+              shadowOpacity: 0.29,
+              shadowRadius: 4.65,
+              marginLeft: 10,
+              marginBottom: 10,
+              marginRight: 10,
+            }}
+            data={results}
+            renderItem={({item}) => (
+              <TouchableOpacity>
+                <Text style={{fontSize: 18, fontWeight: '400', margin: 10}}>
+                  {item.display}
+                </Text>
+              </TouchableOpacity>
+            )}
+          />
+        </View>
+      ) : (
+        <></>
+      )}
     </SafeAreaView>
   );
 };
---
+++ src/service/Api.js
@@ -0,0 +1,8 @@
+export const baseUrl = 'https://tasty.p.rapidapi.com/recipes';
+export const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'a87b341906msh3708714c9202ab7p162accjsn739e666c7c88',
+    'X-RapidAPI-Host': 'tasty.p.rapidapi.com',
+  },
+};
