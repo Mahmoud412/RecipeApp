cccf65be4c82a682d3593376f2daf317bb071749
Mahmoud412 <mahmoudashour219@gmail.com>
2023-03-04 14:03:30 +0300

    added splashScreen

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {Provider} from 'react-redux'
 import store from './src/redux/store';
 import Router from './src/navigation/Router';
@@ -6,9 +6,14 @@ import 'react-native-gesture-handler'
 import { PersistGate } from 'redux-persist/integration/react'
 import {persistStore} from 'redux-persist';
 import Loading from './src/components/Loading';
+import SplashScreen from 'react-native-splash-screen'
+
  let persistor = persistStore(store)
 
 const App = () => {
+  useEffect(() =>{
+    SplashScreen.hide()
+  },[])
   return (
     <Provider store={store}>
       <PersistGate loading={<Loading/>} persistor={persistor}>
